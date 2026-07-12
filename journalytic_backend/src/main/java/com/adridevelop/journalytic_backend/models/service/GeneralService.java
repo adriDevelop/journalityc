7efc5506195ac465f4d3fafd115f4e455c39e46d
Adrian Velasco Carrasco <adridevelop11@gmail.com>
2026-07-12 18:43:06 +0200

    Cambio de ArrayList a List en GeneralService para la correcta devolución de los datos del DAO.

--- journalytic_backend/src/main/java/com/adridevelop/journalytic_backend/models/service/GeneralService.java
+++ journalytic_backend/src/main/java/com/adridevelop/journalytic_backend/models/service/GeneralService.java
@@ -1,10 +1,10 @@
 package com.adridevelop.journalytic_backend.models.service;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public interface GeneralService<T> {
 
-    public ArrayList<T> getAll();
+    public List<T> getAll();
     public T getOneById(Long id);
     public T save(T element);
     public T remove(T element);
